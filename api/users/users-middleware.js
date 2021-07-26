1706db19e170aca15e603067210285adc263c5c1
Michael-Kochis <rhishisikk@gmail.com>
2021-07-26 14:07:09 -0500

    moved user payload verification to users-middleware

---
+++ api/users/users-middleware.js
@@ -0,0 +1,19 @@
+
+const verifyUserPayload = (req, res, next) => {
+    const neoUser = req.body;
+
+    if (!neoUser.username || typeof(neoUser.username) !== "string"
+        || !neoUser.password || typeof(neoUser.password) !== "string"
+        || !neoUser.phoneNumber 
+        || typeof(neoUser.phoneNumber) !== "string" ) {
+        res.status(400).json({ 
+            message: "username, password, and phoneNumber all required" 
+        });
+    }
+
+    next();
+}
+
+module.exports = {
+    verifyUserPayload
+}
