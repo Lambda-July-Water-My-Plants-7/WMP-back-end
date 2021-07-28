008101706171f37b6c085f915a1da5b004f6b2e7
Michael-Kochis <rhishisikk@gmail.com>
2021-07-28 17:48:23 -0500

    rollback multer

--- api/plants/plants-router.js
+++ api/plants/plants-router.js
@@ -1,37 +1,10 @@
 const express = require('express');
-const multer = require('multer');
 const { checkPlantID, secureByOwnerID } = require('./plants-middleware');
 
 const plants = require('./plants-model');
 const router = express.Router();
 
-const fileFilter = (req, file, cb) => {
-    switch(file.mimetype) {
-        case ('image/jpeg'):
-        case ('image/gif'):
-        case ('image/png'): {
-            cb(null, true);
-        }
-        default: 
-            cb(null, false);
-        
-    }
-}
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, path.join(__dirname, './uploads') );
-    },
-    filename: function(req, file, cb) {
-        cb(null, path.extname(file.originalname) );
-    }
-})
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
-});
+
 
 router.get("/", (req, res, next) => {
     const ownerID = req.decoded.id;
@@ -50,13 +23,12 @@ router.get("/:plantID", [checkPlantID, secureByOwnerID],
             }).catch(next);
     })
 
-router.post("/", [upload.single('image')], (req, res, next) => {
+router.post("/", (req, res, next) => {
     const ownerID = req.decoded.id;
     let neoPlant = req.body;
     neoPlant.ownerID = ownerID;
-    neoPlant.image = req.file.path;
-
-    delete neoPlant.userID;
+ 
+    delete neoPlant.userID; //remove userID if sent
 
     plants.createPlant(neoPlant)
         .then(() => {
